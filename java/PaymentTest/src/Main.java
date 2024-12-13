183acf4d15a9e850ce0d077fc53e2f93971c7790
blockenters <58394003+blockenters@users.noreply.github.com>
2024-12-13 17:20:04 +0900

    add

--- java/PaymentTest/src/Main.java
+++ java/PaymentTest/src/Main.java
@@ -1,5 +1,15 @@
+import entity.BankTransferPayment;
+import entity.CardPayment;
+
 public class Main {
     public static void main(String[] args) {
+        // CardPayment 객체 생성 및 테스트
+        CardPayment c1 = new CardPayment("신용카드");
+        c1.processPayment(5000);
 
+        // BankTransferPayment 객체 생성 및 테스트
+        BankTransferPayment b1 = new BankTransferPayment("국민은행");
+        // 각 객체의 processPayment() 메서드 호출
+        b1.processPayment(100000);
     }
 }
--- java/PaymentTest/src/entity/CardPayment.java
+++ java/PaymentTest/src/entity/CardPayment.java
@@ -8,10 +8,9 @@ public class CardPayment extends Payment{
         super("카드");
         this.cardType = cardType;
     }
-
     @Override
     public void processPayment(double amount) {
         // 예: "결제 방식: 카드, 카드 유형: [cardType], 결제 금액: [amount]원"
-        System.out.println("결제 방식: "+paymentMethod+", 카드 유형: "+cardType+", 결제 금액: +"+amount+"+원");
+        System.out.println("결제 방식: "+paymentMethod+", 카드 유형: "+cardType+", 결제 금액: "+amount+"원");
     }
 }
