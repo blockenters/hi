6bce49f5a6986f5836dfb778713374d29f52967e
blockenters <blockenters@gmail.com>
2024-12-20 12:46:06 +0900

    Update UserController.java

--- server/sb-user-server2/java/com/block/user2/controller/UserController.java
+++ server/sb-user-server2/java/com/block/user2/controller/UserController.java
@@ -19,25 +19,33 @@ public class UserController {
     @PostMapping("/api/users")
     public ResponseEntity<UserResDTO> createUser(@RequestBody User user) {
         // 디비에 저장해야 한다. name과 email을 디비에 저장.
-        int result = userDAO.createUser(user);
-        if (result > 0) {
-            // 성공에 대한 응답
-            return ResponseEntity.status(200).body(new UserResDTO("success"));
-        } else {
-            // 실패에 대한 응답
-            return ResponseEntity.status(500).body(new UserResDTO("fail"));
+
+        try{
+
+            int result = userDAO.createUser(user);
+            if (result > 0) {
+                // 성공에 대한 응답
+                return ResponseEntity.status(200).body(new UserResDTO("success"));
+            } else {
+                // 실패에 대한 응답
+                return ResponseEntity.status(500).body(new UserResDTO("fail"));
+            }
+
+        } catch (Exception e) {
+            return ResponseEntity.status(400).build();
         }
+
+
     }
 
     @GetMapping("/api/users/{id}")
     ResponseEntity<UserOneResDTO> getUserById(@PathVariable long id){
         // DB에서 해당 아이디에 해당하는 유저 정보 받아온다.
-        User user = userDAO.getUserById(id);
         // 유저가 있는경우
-        if(user != null){
+        try{
+            User user = userDAO.getUserById(id);
             return ResponseEntity.status(200).body(new UserOneResDTO("success", user));
-        }else {
-            // 유저가 없는경우
+        } catch (Exception e) {
             return ResponseEntity.status(400).build();
         }
 
