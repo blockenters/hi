d996373f9350b10d7f7c3691adad483c325b6374
blockenters <blockenters@gmail.com>
2024-12-24 12:21:42 +0900

    add

--- server/sb-review-server/java/com/block/review/dao/ProductDAO.java
+++ server/sb-review-server/java/com/block/review/dao/ProductDAO.java
@@ -1,12 +1,61 @@
 package com.block.review.dao;
 
+import com.block.review.dto.ProductResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class ProductDAO {
 
-    getAllProducts(int page, int size){
-        String sql = "";
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<ProductResponse> getAllProducts(int page, int size){
+        String sql = "SELECT p.id, p.name, p.description, p.price, p.category, p.stock_quantity, \n" +
+                "\t\tIFNULL( avg(r.rating) , 0  )  as averageRating , \n" +
+                "\t\tcount(r.id) as reviewCount\n" +
+                "from products p\n" +
+                "left join reviews r \n" +
+                "on p.id = r.product_id \n" +
+                "GROUP by p.id\n" +
+                "order by p.id \n" +
+                "limit ? , ? ;";
+        int offset = (page - 1) * size;
+
+        return jdbcTemplate.query(sql, new ProductRowMapper(), offset, size);
+
+    }
+
+
+    public int getTotalElements(){
+        String sql = "select count(*)\n" +
+                "from products;";
+        return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+
+
+    public static class ProductRowMapper implements RowMapper<ProductResponse>{
+
+        @Override
+        public ProductResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+            ProductResponse productResponse = new ProductResponse();
+            productResponse.id = rs.getLong("id");
+            productResponse.name = rs.getString("name");
+            productResponse.description = rs.getString("description");
+            productResponse.price = rs.getInt("price");
+            productResponse.category = rs.getString("category");
+            productResponse.stockQuantity = rs.getInt("stock_quantity");
+            productResponse.averageRating = rs.getDouble("averageRating");
+            productResponse.reviewCount = rs.getInt("reviewCount");
+            return productResponse;
+        }
     }
 
 }
+
--- server/sb-review-server/java/com/block/review/service/ProductService.java
+++ server/sb-review-server/java/com/block/review/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.block.review.service;
 
 import com.block.review.dao.ProductDAO;
+import com.block.review.dto.ProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -11,7 +14,12 @@ public class ProductService {
     ProductDAO productDAO;
 
     getAllProducts(int page, int size){
-            productDAO.getAllProducts(page, size);
+        List<ProductResponse> productList = productDAO.getAllProducts(page, size);
+        int totalElements = productDAO.getTotalElements();
+
+        int totalPages = (int)Math.ceil(totalElements / (double)size);
+        int currentPage = page;
+        int pageSize = size;
     }
 
 }
