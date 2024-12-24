bf857e4ba901960cf032224716a4c9c7b3d8e733
blockenters <blockenters@gmail.com>
2024-12-24 12:56:45 +0900

    add

--- server/sb-review-server/java/com/block/review/controller/ProductController.java
+++ server/sb-review-server/java/com/block/review/controller/ProductController.java
@@ -1,19 +1,24 @@
 package com.block.review.controller;
 
+import com.block.review.dto.ProductListResponse;
 import com.block.review.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @GetMapping("/api/products")
-    getAllProducts(@RequestParam int page, @RequestParam int size){
+    public ResponseEntity<ProductListResponse> getAllProducts(@RequestParam int page, @RequestParam int size){
 
-        productService.getAllProducts(page, size);
+        ProductListResponse productListResponse = productService.getAllProducts(page, size);
+        return ResponseEntity.status(200).body(productListResponse);
 
     }
 
--- server/sb-review-server/java/com/block/review/service/ProductService.java
+++ server/sb-review-server/java/com/block/review/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.block.review.service;
 
 import com.block.review.dao.ProductDAO;
+import com.block.review.dto.ProductListResponse;
 import com.block.review.dto.ProductResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,13 +14,15 @@ public class ProductService {
     @Autowired
     ProductDAO productDAO;
 
-    getAllProducts(int page, int size){
+    public ProductListResponse getAllProducts(int page, int size){
         List<ProductResponse> productList = productDAO.getAllProducts(page, size);
         int totalElements = productDAO.getTotalElements();
-
         int totalPages = (int)Math.ceil(totalElements / (double)size);
         int currentPage = page;
         int pageSize = size;
+
+        ProductListResponse productListResponse = new ProductListResponse(productList, totalPages, totalElements, currentPage, pageSize);
+        return productListResponse;
     }
 
 }
